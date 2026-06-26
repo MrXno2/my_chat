f438bcb77524900d59c933ddc2e6477dc7ff6c62
MrXno2 <180126373+MrXno2@users.noreply.github.com>
2026-06-27 01:54:10 +1200

    pravki melkie

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,7 +4,6 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DATABASE_URL: str
     JWT_SECRET_KEY: str
-    BCRYPT_KEY: str
     cors_allowed_origins: list[str] = ["http://localhost:3000"]
 
     class Config:
